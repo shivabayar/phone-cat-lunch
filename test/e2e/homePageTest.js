9b5b24ede567f4bb16c2d96d258291fffbcd10a0
shiva <shivraj.s@hashedin.com>
2015-04-10 12:18:04 +0530

    methods to verify url in home page obj

--- test/e2e/homePageTest.js
+++ test/e2e/homePageTest.js
@@ -1,19 +1,38 @@
 describe('Home Page', function () {
     
     "use strict";
-    var hp = require('../e2e/support/homePageObj.js'),
+    var homePage = require('../e2e/support/homePageObj.js'),
+    homeDetailPage = require('../e2e/support/phoneDetailPageObj.js'),
     ptor;
 
     beforeEach(function () {
         ptor = protractor.getInstance();
-        hp.get();
+        homePage.getPage();
     });
 
     it('Enter nexus and count the search results', function () {
-        hp.searchBox.sendKeys("nexus");
-        hp.getSearchResultsCount().then(function (count) {
+        homePage.searchBox.sendKeys("nexus");
+
+        homePage.getSearchResultsCount().then(function (count) {
             expect(count).toBe(1);
         });
-        ptor.sleep(3000);
+        homePage.clickFirstSearchResult();
+        homePage.verifyUrl('/phones/nexus-s');
+        ptor.sleep(2000);
+    });
+
+    it('Enter nexus and count the search results', function () {
+        homePage.searchBox.sendKeys("motorola");
+
+        homePage.getSearchResultsCount().then(function (count) {
+            expect(count).toBe(8);
+        });
+        homePage.clickFirstSearchResult();
+        ptor.sleep(2000);
+    });
+
+    it('should go to nexus page', function () {
+        homeDetailPage.gotoPage('/phones/nexus-s');
+        ptor.sleep(2000);
     });
 });
--- test/e2e/support/homePageObj.js
+++ test/e2e/support/homePageObj.js
@@ -4,13 +4,23 @@ var homePageObj = function () {
 
     this.searchBox = element(by.model('query'));
 
-    this.get = function () {
+    this.getPage = function () {
         browser.get('http://localhost:8000');
     };
     
     this.getSearchResultsCount = function () {
         var phoneList = element.all(by.repeater('phone in phones'));
         return phoneList.count();
+    };
+    
+    this.clickFirstSearchResult = function () {
+        element.all(by.css('.phones li a')).first().click();  
+    };
+    
+    this.verifyUrl = function (urlToMatch) {
+        browser.getLocationAbsUrl().then(function(url) {
+            expect(url.split('#')[1]).toBe(urlToMatch);
+        });
     };
 };
 
